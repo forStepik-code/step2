f84a60c04d91ccdb0e2142104e86d3d1cba2c773
stas <stasek@gmail.com>
2019-06-15 16:49:08 +0300

    Add tests for basket button

---
+++ conftest.py
@@ -0,0 +1,20 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default="en",
+                     help="Give me language")
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    language = request.config.getoption("language")
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': language})
+    wd = webdriver.Chrome(options=options)
+    wd.implicitly_wait(5)
+    yield wd
+    print("\nquit browser..")
+    wd.quit()
---
+++ test_items.py
@@ -0,0 +1,12 @@
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    browser.get(link)
+    basket = browser \
+        .find_element_by_xpath("//button[contains(@class, 'btn-add-to-basket')]")
+
+    # time.sleep(30)
+    assert EC.visibility_of(basket), "There is not basket button"
